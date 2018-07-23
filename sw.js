6797fc8e3addf03c7512b85f95c0ca120762c810
Stefanie Chin <hi.stef.c@gmail.com>
2018-07-22 22:35:33 -0700

    feat: add install event listener, sw

--- sw.js
+++ sw.js
@@ -1,18 +1,47 @@
+let cacheName = 'offline cache'; //sets versions for our cache
+let cacheFiles = [
+	'/',
+	'/index.html',
+	'/restaurant.html',
+	'css/styles.css',
+	'js/dbhelper.js',
+	'js/main.js',
+	'js/restaurant_info.js',
+]
 
-	self.addEventListener('install', event => console.log('[ServiceWorker] Installed'));
+	self.addEventListener('install', event => {
+		console.log('[ServiceWorker] Installed');
+			//install event has to wait until promise is resolved
+		event.waitUntil(
+			caches.open(cacheName) //caches API
+			.then(cache => {
+				console.log('[ServiceWorker] Caching cacheFiles');
+				return cache.addAll(cacheFiles);
+				})
+			.catch( error => console.log('error: here"s the problem'))
+			)
+	});
 
 	self.addEventListener('fetch', event => {
 		console.log('[ServiceWorker] fetched');
+
 		event.respondWith(
-			fetch(event.request).then(()=>{
-				if (response.status === 404){
-					return new Response("Non-existent page");
-				}
-				return response;
-			}).catch(()=>{
-				return new Response('Server connection failed');				
-			})
-		);
+
+		    caches.match(event.request).then(function(response) {
+		      if (response) {
+		       return response;
+		      } return fetch(event.request);
+		    })
+	    );			
+		// 	fetch(event.request).then(response=>{
+		// 		if (response.status === 404){
+		// 			return new Response("Non-existent page");
+		// 		}
+		// 		return response;
+		// 	}).catch(()=>{
+		// 		return new Response('Server connection failed');				
+		// 	})
+		// );
 	});
 
 	self.addEventListener('activate', event => {
