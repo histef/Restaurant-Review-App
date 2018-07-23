d5ec6510d8453f37ab3f6adf2ccdf7219611c8f2
Stefanie Chin <hi.stef.c@gmail.com>
2018-07-22 23:18:32 -0700

    feat: add more files to sw cache

--- sw.js
+++ sw.js
@@ -4,9 +4,21 @@ let cacheFiles = [
 	'/index.html',
 	'/restaurant.html',
 	'css/styles.css',
+	'js/register-sw.js',
 	'js/dbhelper.js',
 	'js/main.js',
-	'js/restaurant_info.js'
+	'js/restaurant_info.js',
+	'img/1.jpg',
+	'img/2.jpg',
+	'img/3.jpg',
+	'img/4.jpg',
+	'img/5.jpg',
+	'img/6.jpg',
+	'img/7.jpg',
+	'img/8.jpg',
+	'img/9.jpg',
+	'img/10.jpg',
+	'data/restaurants.json'
 ]
 
 self.addEventListener('install', event => {
@@ -18,7 +30,7 @@ self.addEventListener('install', event => {
 			console.log('[ServiceWorker] Caching cacheFiles');
 			return cache.addAll(cacheFiles);
 			})
-		.catch( error => console.log('error: here"s the problem'))
+		.catch( error => console.log('error: problem'))
 	)
 });
 
@@ -55,5 +67,5 @@ self.addEventListener('activate', event => {
 				return caches.delete(thisCacheName);
 			}
 		})))
-	)
+	);
 });
